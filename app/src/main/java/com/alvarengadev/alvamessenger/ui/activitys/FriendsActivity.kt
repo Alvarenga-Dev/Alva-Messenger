f73ede9006733e8479df8413ab4cab5c42f2979a
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-03-26 10:04:25 -0300

    Changing Class FriendsActivity

--- app/src/main/java/com/alvarengadev/alvamessenger/ui/activitys/FriendsActivity.kt
+++ app/src/main/java/com/alvarengadev/alvamessenger/ui/activitys/FriendsActivity.kt
@@ -2,10 +2,12 @@ package com.alvarengadev.alvamessenger.ui.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.alvarengadev.alvamessenger.R
+import com.alvarengadev.alvamessenger.utils.RoutersUtils
 import kotlinx.android.synthetic.main.activity_friends.*
 
-class FriendsActivity : AppCompatActivity() {
+class FriendsActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +16,23 @@ class FriendsActivity : AppCompatActivity() {
         initToolbar();
     }
 
-    fun initToolbar() {
+    private fun initToolbar() {
         toolbarFriends.title = "";
         setSupportActionBar(toolbarFriends)
+        toolbarFriends.setNavigationIcon(R.drawable.ic_arrow_back)
+        toolbarFriends.setNavigationOnClickListener(this)
+    }
+
+    private fun routerBackHome() {
+        startActivity(RoutersUtils.router(applicationContext, HomeActivity::class.java))
+        finish()
+    }
+
+    override fun onClick(view: View) {
+        routerBackHome()
+    }
+
+    override fun onBackPressed() {
+        routerBackHome()
     }
 }
