c3087acdfe85e25dcf622e574f176c5d6771173a
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-04-03 18:46:57 -0300

    Refactoring the code

--- app/src/main/java/com/alvarengadev/alvamessenger/models/User.kt
+++ app/src/main/java/com/alvarengadev/alvamessenger/models/User.kt
@@ -4,18 +4,16 @@ import com.google.firebase.database.Exclude
 
 class User {
 
-
-
     @get:Exclude
-    var id:String = ""
-    var firstName:String = ""
-    var lastName:String = ""
-    var email:String = ""
+    var id: String = ""
+    var firstName: String = ""
+    var lastName: String = ""
+    var email: String = ""
     @get:Exclude
-    var password:String = ""
+    var password: String = ""
 
     constructor()
-    constructor(firstName:String, lastName:String, email:String, password:String) {
+    constructor(firstName: String, lastName: String, email: String, password: String) {
         this.firstName = firstName
         this.lastName = lastName
         this.email = email
