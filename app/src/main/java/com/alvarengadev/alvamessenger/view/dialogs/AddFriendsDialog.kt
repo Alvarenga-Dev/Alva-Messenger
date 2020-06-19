c502ff5c85719430b2ab1c410ed77054a9ac57d4
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-06-19 08:32:22 -0300

    chore: Refactor code; Change error handling for the user

--- app/src/main/java/com/alvarengadev/alvamessenger/view/dialogs/AddFriendsDialog.kt
+++ app/src/main/java/com/alvarengadev/alvamessenger/view/dialogs/AddFriendsDialog.kt
@@ -5,25 +5,30 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatDialogFragment
 import com.alvarengadev.alvamessenger.R
-import com.alvarengadev.alvamessenger.presenter.friend.AddFriend
+import com.alvarengadev.alvamessenger.presenter.friend.add.AddFriendInterface
+import com.alvarengadev.alvamessenger.presenter.friend.add.AddFriendPresenter
 import com.alvarengadev.alvamessenger.utils.InputsValidatorUtils
+import com.alvarengadev.alvamessenger.utils.PreferencesUtils
 import com.google.android.material.textfield.TextInputLayout
 
-class AddFriendsDialog : AppCompatDialogFragment() {
+class AddFriendsDialog : AppCompatDialogFragment(), AddFriendInterface.View {
+
+    private lateinit var inputEmail: TextInputLayout
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
         val builder = AlertDialog.Builder(this.activity)
         val layoutInflater = activity?.layoutInflater
-        val view = layoutInflater?.inflate(R.layout.dialog_add_friends, null)
+        val view = layoutInflater?.inflate(R.layout.dialog_add_friends, null)!!
         builder.setView(view)
 
-        val btnDismiss = view!!.findViewById(R.id.btnDismissAddFriendsDialog) as Button
+        val btnDismiss = view.findViewById(R.id.btnDismissAddFriendsDialog) as Button
         val btnAddFriends = view.findViewById(R.id.btnAddFriendsDialog) as Button
-        val inputEmail = view.findViewById(R.id.inputAddFriendEmail) as TextInputLayout
+        inputEmail = view.findViewById(R.id.inputAddFriendEmail) as TextInputLayout
 
         val dialog = builder.create()
         dialog.setCanceledOnTouchOutside(false)
@@ -41,11 +46,21 @@ class AddFriendsDialog : AppCompatDialogFragment() {
 
     private fun addFriend(inputEmail: TextInputLayout, email: String) {
         val inputsValidatorUtils = InputsValidatorUtils(context!!)
-        if ( inputsValidatorUtils.validateEmail(inputEmail, email) ) {
-            val addFriend = AddFriend(context!!, email)
-            addFriend.add()
-            dialog?.dismiss()
+        if (inputsValidatorUtils.validateEmail(inputEmail, email)) {
+            AddFriendPresenter(this, email).addFriend()
         }
-
     }
+
+    override fun getUserKey(): String? =
+        PreferencesUtils(context!!).getUserKey()
+
+    override fun error(message: String) {
+        Log.e("Usuário", "Não existe! $message")
+        inputEmail.error = message
+    }
+
+    override fun closeDialog(isAdd: Boolean) {
+        if (isAdd) dialog?.dismiss()
+    }
+
 }
