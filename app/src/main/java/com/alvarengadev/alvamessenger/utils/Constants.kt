e28e435e25516591f91eac4e5a64e01229908579
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-07-22 17:31:30 -0300

    chore: add news constants

--- app/src/main/java/com/alvarengadev/alvamessenger/utils/Constants.kt
+++ app/src/main/java/com/alvarengadev/alvamessenger/utils/Constants.kt
@@ -2,6 +2,12 @@ package com.alvarengadev.alvamessenger.utils
 
 class Constants {
     companion object {
+        const val FIREBASE_CHILD_USERS = "users"
+        const val FIREBASE_CHILD_CHATS = "chats"
+        const val FIREBASE_CHILD_FRIENDS = "friends"
+        const val FIREBASE_CHILD_MESSAGES = "messages"
+        const val FIREBASE_STORAGE_CHILD_IMAGES = "images"
+        const val FIREBASE_STORAGE_CHILD_PROFILES = "profiles"
         const val FRIEND_NAME = "friend.name.alvamessenger"
         const val FRIEND_EMAIL = "friend.email.alvamessenger"
         const val REQUEST_PERMISSION_ENABLE = 1
