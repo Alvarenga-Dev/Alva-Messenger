142ff6ddf35a8226d46a53b8c69d0e1411bb13c6
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-07-22 15:25:35 -0300

    chore: add Storage Reference

--- app/src/main/java/com/alvarengadev/alvamessenger/data/firebase/SettingsFirebase.kt
+++ app/src/main/java/com/alvarengadev/alvamessenger/data/firebase/SettingsFirebase.kt
@@ -3,10 +3,13 @@ package com.alvarengadev.alvamessenger.data.firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 abstract class SettingsFirebase{
     companion object {
         val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().reference
         val authReference: FirebaseAuth = FirebaseAuth.getInstance()
+        val storageReference: StorageReference = FirebaseStorage.getInstance().reference
     }
 }
